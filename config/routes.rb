ba81760f95abe4902c2c5c1e7601f58ed4bf8b16
cupatea <vdavyden@gmail.com>
2018-01-08 02:42:03 +0200

    edit routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  root to: 'chats#index'
+  devise_for :users
+  resources :chats, only: [:index, :show]
+  resources :messages, only: [:create]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
