1df38ef11de946c587aa411d16b68692256e8b6c
cupatea <vdavyden@gmail.com>
2018-01-08 02:38:10 +0200

    add user scopes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,14 @@
 class User < ApplicationRecord
-  has_many :messages
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
+  has_many :sended_messages,   class_name: 'Message', foreign_key: :addresser_id
+  has_many :received_messages, class_name: 'Message', foreign_key: :addressee_id
+
+  def has_messages_from id
+    received_messages.where addresser_id: id
+  end
+
 end
