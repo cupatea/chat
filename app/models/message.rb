bc744abd54a2b76aa259522f938534246a82e832
cupatea <vdavyden@gmail.com>
2018-01-08 02:38:32 +0200

    add message scope

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,4 +2,8 @@ class Message < ApplicationRecord
   belongs_to :addressee, class_name: 'User'
   belongs_to :addresser, class_name: 'User'
 
+  def self.between(first_id, second_id)
+    where(addressee_id: first_id, addresser_id: second_id)
+    .or(where(addressee_id: second_id, addresser_id: first_id)).order(:created_at)
+  end
 end
