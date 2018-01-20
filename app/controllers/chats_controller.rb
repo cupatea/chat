be65956ec03e71405c9acd08b9a40773ee5f3635
cupatea <vdavyden@gmail.com>
2018-01-20 20:38:59 +0200

    remove odd variables

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -2,14 +2,12 @@ class ChatsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @users = User.all - [ current_user ]
+    @users = User.all_except current_user
     @messages_count = current_user.has_messages_from.count
   end
 
   def show
-    @addressee = User.find_by_id params[:id]
-    @new_message =  Message.new addressee_id: @addressee.id
-    redirect_to chats_path unless @addressee
+    redirect_to chats_path unless User.exists? id: params[:id]
   end
 
   def messages
