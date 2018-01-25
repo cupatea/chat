136f6fecdce9f896669c6d1841a608f1818d0c0f
cupatea <vdavyden@gmail.com>
2018-01-25 18:54:11 +0200

    add comment

--- app/jobs/user_relay_job.rb
+++ app/jobs/user_relay_job.rb
@@ -1,6 +1,7 @@
 class UserRelayJob < ApplicationJob
   queue_as :default
-
+  # Broadcast specific information about user to the 'users' stream. This job suppose to be executed in a case of any updates connected with user.
+  # E.g. user become online or offline
   def perform(user)
     ActionCable.server.broadcast "users", { name: user.name, id: user.id , online: user.online, last_seen_at: user.updated_at }
   end
