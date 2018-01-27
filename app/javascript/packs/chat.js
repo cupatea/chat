f808d0cbe790962503236b96901e95e3ce55a1da
cupatea <vdavyden@gmail.com>
2018-01-27 21:17:47 +0200

    fix scroll for a case if incoming message in a viewport

--- app/javascript/packs/chat.js
+++ app/javascript/packs/chat.js
@@ -15,6 +15,16 @@ axios.defaults.headers.common = {
 }
 
 class Chat extends Component {
+  static isInViewport(element) {
+    const rect = element.getBoundingClientRect()
+    const html = document.documentElement
+    return (
+      rect.top >= 0 &&
+      rect.left >= 0 &&
+      rect.bottom <= (window.innerHeight || html.clientHeight) &&
+      rect.right <= (window.innerWidth || html.clientWidth)
+    )
+  }
   constructor(props) {
     super(props)
     this.state = {
@@ -98,6 +108,9 @@ class Chat extends Component {
               : 1),
           },
         })
+        if (this.state.roomId === room && Chat.isInViewport(this.messagesList.lastChild)) {
+          this.scrollToBottom()
+        }
       },
     })
   }
